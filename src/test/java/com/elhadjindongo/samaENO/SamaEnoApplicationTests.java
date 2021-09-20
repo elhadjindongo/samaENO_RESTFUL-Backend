0587637409c0ff06692f9f21f0fc669a5f3005da
El Hadji M. NDONGO <ndongoel@gmail.com>
2021-09-20 13:43:04 +0000

    Initial commit

---
+++ src/test/java/com/elhadjindongo/samaENO/SamaEnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhadjindongo.samaENO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamaEnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
